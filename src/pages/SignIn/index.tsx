35c0ba84d1b6cab10898da5e47a99ae90684c20b
DavilsonJunior <davilsonjr_jobs@yahoo.com>
2022-04-28 11:17:09 -0300

    added Navigation between screens SignIn and SignUp

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,66 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+import { theme } from '../../styles/theme';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-    <Title>Faça seu logon</Title>
+const SignIn: React.FC = () => {
+  const navigation = useNavigation();
 
-    <Input name="email" icon="mail" placeholder="E-mail" />
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
 
-    <Input name="password" icon="lock" placeholder="Senha" />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
 
-    <Button onPress={() => {}}>Entrar</Button>
-  </Container>
-);
+          <Input name="email" icon="mail" placeholder="E-mail" />
+
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => {}}>Entrar</Button>
+
+          <ForgotPassword>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+
+          <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+            <Icon name="log-in" size={20} color={theme.colors.primary} />
+            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+          </CreateAccountButton>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => <Container />;
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+import { theme } from '../../styles/theme';
+
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+
+          <Input name="email" icon="mail" placeholder="E-mail" />
+
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => {}}>Entrar</Button>
+
+          <BackToSignIn onPress={() => navigation.goBack()}>
+            <Icon name="arrow-left" size={20} color={theme.colors.text} />
+            <BackToSignInText>Voltar para logon</BackToSignInText>
+          </BackToSignIn>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default SignUp;
